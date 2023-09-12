de75f711ce20936afc7ce544b06ba9a561b33779
RavichandraYellenki <134284687+RavichandraYellenki@users.noreply.github.com>
2023-09-12 17:41:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateWindowSize(){
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+
+	document.getElementById('h1tag').innerText =`Width: ${width} and Height: ${height} `;
+}
+updateWindowSize();
+
+window.addEventListener('resize',updateWindowSize);
